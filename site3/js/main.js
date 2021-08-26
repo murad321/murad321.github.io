4d9111afa08377a9db1a1ffc576eff4145594394
murad321 <56887859+murad321@users.noreply.github.com>
2021-08-27 00:15:21 +0400

    main

--- site3/js/main.js
+++ site3/js/main.js
@@ -92,14 +92,15 @@ if (isMobile.any()) {
 //Меню бургер
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
+const menuLogoContainer = document.querySelector('.menu-logo__container');
 if (iconMenu){
     iconMenu.addEventListener("click", function(e) {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
+        menuLogoContainer.classList.toggle('_active');
     });
 }
-
 
 
 
