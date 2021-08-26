6a7b1fb4870606352c94ed81c811025f73fe39f6
murad321 <56887859+murad321@users.noreply.github.com>
2021-08-27 03:12:24 +0400

    main

--- site3/js/main.js
+++ site3/js/main.js
@@ -17,6 +17,7 @@ $(function(){
  $('.heroes__slider-img').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay:true,
   asNavFor: '.heroes__slider-text',
   prevArrow:'<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
   nextArrow:'<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
